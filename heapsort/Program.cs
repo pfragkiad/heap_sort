0811effef0559eb437988041b592ca7c3cb061b8
Pavlos Fragkiadoulakis <=>
2023-11-03 01:43:05 +0200

    Add project files.

--- heapsort/Program.cs
+++ heapsort/Program.cs
@@ -1,15 +1,25 @@
-﻿int[] a = new[] { 6, 3, 7, 9, 12, 17, 1, 14, 20 };
-int level = 1, remaining = a.Length, currentIndex = 0;
+﻿using System.Text;
 
-List<string> levels = new();
-while (remaining > 0)
+public static class Program
 {
-    int maxLevelItems = (int)Math.Pow(2, level - 1);
-    int levelItems = Math.Min(maxLevelItems, remaining);
-    for (int i = currentIndex; i < currentIndex + levelItems; i++)
-        Console.Write($"{a[i]} ");
-    currentIndex += levelItems;
-    remaining -= levelItems;
-    Console.WriteLine();
-    level++;
+    private static void Main(string[] args)
+    {
+        int[] a = new[] { 6, 3, 7, 9, 12, 17, 1, 14, 20 };
+        int level = 1, remaining = a.Length, currentIndex = 0;
+
+        Dictionary<int, int[]> levels = new();
+        while (remaining > 0)
+        {
+            int maxLevelItems = (int)Math.Pow(2, level - 1);
+            int levelItems = Math.Min(maxLevelItems, remaining);
+
+            levels.Add(level,a.Skip(currentIndex).Take(levelItems).ToArray());
+            Console.WriteLine(string.Join(" ",levels[level]));
+            
+            currentIndex += levelItems;
+            remaining -= levelItems;
+            level++;
+           
+        }
+    }
 }
